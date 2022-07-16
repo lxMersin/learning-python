275d776f1d27f608199a6152daa01e1b309a6fcd
Emerson <86484423+lxMersin@users.noreply.github.com>
2022-07-16 08:53:43 -0300

    Add files via upload

---
+++ expy/script.js
@@ -0,0 +1,176 @@
+function inicializa(){
+    document.querySelectorAll('main article section')[0].style.display = "block";
+    document.querySelectorAll('main article section')[1].style.display = "none";
+    document.querySelectorAll('main article section')[2].style.display = "none";
+    document.querySelectorAll('main article section')[3].style.display = "none";
+    document.querySelectorAll('main article section')[4].style.display = "none";
+    document.getElementById('logado').style.display = "none";
+
+    //button
+    var button = document.getElementsByTagName('button')[0];
+    button.style.background = "#000000"
+    button.style.color = "#42C920"
+}
+function home(){
+    document.querySelectorAll('main article section')[0].style.display = "block";
+    document.querySelectorAll('main article section')[1].style.display = "none";
+    document.querySelectorAll('main article section')[2].style.display = "none";
+    document.querySelectorAll('main article section')[3].style.display = "none";
+    document.querySelectorAll('main article section')[4].style.display = "none";
+
+    //buttons
+    var button = document.getElementsByTagName('button')[0];
+    button.style.background = "#000000"
+    button.style.color = "#42C920"
+
+    //Login
+    var button = document.getElementsByTagName('button')[1];
+    button.style.background = "#42C920"
+    button.style.color = "#000000"
+
+    //Cadastrar
+    var button = document.getElementsByTagName('button')[2];
+    button.style.background = "#42C920"
+    button.style.color = "#000000"
+
+    //info
+    var button = document.getElementsByTagName('button')[3];
+    button.style.background = "#42C920"
+    button.style.color = "#000000"
+
+    //info2
+    var button = document.getElementsByTagName('button')[4];
+    button.style.background = "#42C920"
+    button.style.color = "#000000"
+
+
+}
+function login(){
+    document.querySelectorAll('main article section')[0].style.display = "none";
+    document.querySelectorAll('main article section')[1].style.display = "block";
+    document.querySelectorAll('main article section')[2].style.display = "none";
+    document.querySelectorAll('main article section')[3].style.display = "none";
+    document.querySelectorAll('main article section')[4].style.display = "none";
+
+    //buttons
+    var button = document.getElementsByTagName('button')[0];
+    button.style.background = "#42C920"
+    button.style.color = "#000000"
+
+    //Login
+    var button = document.getElementsByTagName('button')[1];
+    button.style.background = "#000000"
+    button.style.color = "#42C920"
+
+    //Cadastrar
+    var button = document.getElementsByTagName('button')[2];
+    button.style.background = "#42C920"
+    button.style.color = "#000000"
+    
+    //info
+    var button = document.getElementsByTagName('button')[3];
+    button.style.background = "#42C920"
+    button.style.color = "#000000"
+    
+    //info2
+    var button = document.getElementsByTagName('button')[4];
+    button.style.background = "#42C920"
+    button.style.color = "#000000"
+}
+
+function cadastro(){
+    document.querySelectorAll('main article section')[0].style.display = "none";
+    document.querySelectorAll('main article section')[1].style.display = "none";
+    document.querySelectorAll('main article section')[2].style.display = "block";
+    document.querySelectorAll('main article section')[3].style.display = "none";
+    document.querySelectorAll('main article section')[4].style.display = "none";
+
+    //buttons
+    var button = document.getElementsByTagName('button')[0];
+    button.style.background = "#42C920"
+    button.style.color = "#000000"
+
+    //Login
+    var button = document.getElementsByTagName('button')[1];
+    button.style.background = "#42C920"
+    button.style.color = "#000000"
+
+    //Cadastrar
+    var button = document.getElementsByTagName('button')[2];
+    button.style.background = "#000000"
+    button.style.color = "#42C920"
+    //info
+    var button = document.getElementsByTagName('button')[3];
+    button.style.background = "#42C920"
+    button.style.color = "#000000"
+
+    //info2
+    var button = document.getElementsByTagName('button')[4];
+    button.style.background = "#42C920"
+    button.style.color = "#000000"
+}
+
+function info(){
+    document.querySelectorAll('main article section')[0].style.display = "none";
+    document.querySelectorAll('main article section')[1].style.display = "none";
+    document.querySelectorAll('main article section')[2].style.display = "none";
+    document.querySelectorAll('main article section')[3].style.display = "block";
+    document.querySelectorAll('main article section')[4].style.display = "none";
+
+    //buttons
+    var button = document.getElementsByTagName('button')[0];
+    button.style.background = "#42C920"
+    button.style.color = "#000000"
+
+    //Login
+    var button = document.getElementsByTagName('button')[1];
+    button.style.background = "#42C920"
+    button.style.color = "#000000"
+
+    //Cadastrar
+    var button = document.getElementsByTagName('button')[2];
+    button.style.background = "#42C920"
+    button.style.color = "#000000"
+    //info
+    var button = document.getElementsByTagName('button')[3];
+    button.style.background = "#000000"
+    button.style.color = "#42C920"
+
+    //info2
+    var button = document.getElementsByTagName('button')[4];
+    button.style.background = "#42C920"
+    button.style.color = "#000000"    
+
+}
+function infodois(){
+    document.querySelectorAll('main article section')[0].style.display = "none";
+    document.querySelectorAll('main article section')[1].style.display = "none";
+    document.querySelectorAll('main article section')[2].style.display = "none";
+    document.querySelectorAll('main article section')[3].style.display = "none";
+    document.querySelectorAll('main article section')[4].style.display = "block";
+
+    //buttons
+    var button = document.getElementsByTagName('button')[0];
+    button.style.background = "#42C920"
+    button.style.color = "#000000"
+
+    //Login
+    var button = document.getElementsByTagName('button')[1];
+    button.style.background = "#42C920"
+    button.style.color = "#000000"
+
+    //Cadastrar
+    var button = document.getElementsByTagName('button')[2];
+    button.style.background = "#42C920"
+    button.style.color = "#000000"
+    //info
+    var button = document.getElementsByTagName('button')[3];
+    button.style.background = "#42C920"
+    button.style.color = "#000000"
+
+    //info2
+    var button = document.getElementsByTagName('button')[4];
+    button.style.background = "#000000"
+    button.style.color = "#42C920"    
+
+}
